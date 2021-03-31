15a519a8958e3db4541e08c116a7cbb788d8f4a2
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-03-31 11:36:43 +0000

    added javascript if logo doesn't load display title text instead of broken image

--- static/js/base.js
+++ static/js/base.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
     if (topOfFooter < window.innerHeight){
         $('#footer').addClass('fixed-bottom');
     }
+    // Logo image loaded - if error, display title text
+    $("#topLogo")
+        .on('load', function() { 
+            // Loaded Successfully 
+        })
+        .on('error', function() { 
+            $(this).hide();
+            $('#titleText').removeClass('hidden');
+        });
 });
