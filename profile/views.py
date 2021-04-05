0fee05630a939d97df3bfeaf2d3a48363e6a9886
J-O-B <Jonathanmichaelobrien@gmail.com>
2021-04-05 12:30:43 +0000

    work on user profile

--- profile/views.py
+++ profile/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .forms import UserUpdateForm
-from .models import Profile
+from .models import Profile, create_or_update_user_profile
 from django.contrib import messages
+
+from datetime import date
+
+today = date.today()
 
 
 @login_required
@@ -11,9 +15,13 @@ def profile(request):
     """
     View To Return Homepage
     """
-    profile = Profile.objects
-    current_user = request.user
-    user = current_user.id
+    user = request.user
+    if user:
+        profile = "found"
+        create_or_update_user_profile(
+            sender=user, instance=user, created=today)
+    else:
+        profile = "not found"
 
     template = "profile/home.html"
     form = UserUpdateForm()
